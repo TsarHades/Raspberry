a33cfe675f662c76db1692ec9f35e56c4fa767e5
PC-NICOLAS\nicol <nicolasjacobs2407@gmail.com>
2024-05-10 13:27:54 +0200

    Raspberry pi 1.5
    Changelog:
    - base loop gemaakt
    - de arduino base gemaakt, gewoon nog goed maken als ik uitvogel hoe arduino überhaupt werkt

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ resultsoutput =[]
 V1 = 0
 V2 = 0
 CalibrateZero=0
-weight = False
+weighttest = False
 BLYNK_TEMPLATE_ID = "TMPL5E9rUAxOQ"
 BLYNK_TEMPLATE_NAME = "Quickstart Template"
 BLYNK_AUTH_TOKEN = "WJbSxpdW_KDfcH1BnAtdHlX6vxxeAXRZ"
@@ -41,6 +41,7 @@ def getav(numbers):
         for i in range(x,y):
             avgweights += average[i]
         result = (avgweights/6)-CalibrateZero
+        result = (calweight*result)/(CalibrateWeight-CalibrateZero) #Lineair interpolatie naar gewicht
         resultsoutput.append(result)
         x = y
         y += 6
@@ -69,30 +70,50 @@ if __name__ == '__main__':
         @blynk.on("V1")
         def V1_handler(value):
                 if value == 1:
+                    global CalibrateZero
                     CalibrateZero = qwiic.getAverage(averageAmount=64)
                 if value == 0:
                     blynk.virtual_write("V0",0)
-                    V1 = 0
+                    global V1
+                    V1 = 1
 
     blynk.virtual_write("V2,1")
     while V1 != 1:
         @blynk.on("V1")
         def V2_handler(value):
             if value == 1:
+                global CalibrateWeight
                 CalibrateWeight = qwiic.getAverage(averageAmount=64)
-                while weight == False:
+                while weighttest == False:
                     @blynk.on("V3")
                     def V3_handler(weight):
+                        global calweight
                         calweight = weight
-                        weight= True
+                        global weighttest
+                        weighttest = True
             if value ==0:
                 blynk.virtual_write("V2",0)
+                global V1
+                V1 = 1
+    @blynk.on("V1")
+    def V1_handler2(value):
+        global motor
+        motor = value
 
     while True:
         blynk.run()
         numbers = getav(qwiic.getReading())
         for i in range(0,len(numbers)):
             blynk.virtual_write("V4",numbers[i])
+        if motor == 0:
+        #Ga omhoog arduino
+        if motor == 1:
+        #Stop arduino
+        if motor == 2:
+        #Ga omlaag arduino
+
+
+
 
 
 
